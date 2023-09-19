358b7f507d496ad5c24b46beea9968fbc41334b5
Stefan Richter <stefan@chkk.io>
2023-09-19 11:09:54 -0700

    Split out personal and other accounts and add callouts feature
    Splitting out the set of accounts into personal and other accounts. With that move the listing cut off to amount instead of count. And add a set of callouts every tuesday to shame people into saving money.

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 import datetime
+import json
 import logging
 import os
 from collections import defaultdict
-from typing import Dict, List, Optional
+from typing import Dict, List, Tuple
 
 import boto3
 import requests
@@ -44,17 +45,16 @@ def find_by_key(values: list, key: str, value: str):
 
 
 def lambda_handler(event, context):
-    length = int(os.environ.get("LENGTH", "15"))
     cost_aggregation = os.environ.get("COST_AGGREGATION", "UnblendedCost")
 
-    buffer = report_cost(max_entries=length, cost_aggregation=cost_aggregation)
+    personal_buffer, other_buffer, callout_users = report_cost(cost_floor=20, cost_aggregation=cost_aggregation)
 
     slack_hook_url = os.environ.get('SLACK_WEBHOOK_URL')
     if slack_hook_url:
-        publish_slack(slack_hook_url, buffer)
+        publish_slack(slack_hook_url, personal_buffer, other_buffer, callout_users)
 
 
-def report_cost(max_entries: int, cost_aggregation: str = "UnblendedCost"):
+def report_cost(cost_floor: int, cost_aggregation: str = "UnblendedCost") -> Tuple[str, str, List[str]]:
     today = datetime.datetime.today()
     a_week_ago = today - datetime.timedelta(days=7)
     two_months_ago = today - datetime.timedelta(days=70)
@@ -148,30 +148,68 @@ def report_cost(max_entries: int, cost_aggregation: str = "UnblendedCost"):
             cost_per_day_dict[key].append(cost)
         month_count += 1
 
+    # Split out personal and other accounts
+    personal_cost_per_month_dict: Dict[str, List[float]] = defaultdict(list)
+    other_cost_per_month_dict: Dict[str, List[float]] = defaultdict(list)
+    for key, value in cost_per_month_dict.items():
+        if "personal-" in key:
+            personal_cost_per_month_dict[key] = value
+        else:
+            other_cost_per_month_dict[key] = value
+
     # Sort the map by yesterday's cost
-    most_expensive = sorted(cost_per_month_dict.items(), key=lambda i: i[1][0], reverse=True)
+    personal_most_expensive = sorted(personal_cost_per_month_dict.items(), key=lambda i: i[1][0], reverse=True)
+    other_most_expensive = sorted(other_cost_per_month_dict.items(), key=lambda i: i[1][0], reverse=True)
     longest_name_len = len(max(cost_per_month_dict.keys(), key = len))
 
-    buffer = f"{'AWS Accounts':^{longest_name_len}} | Month-to-date | yesterday | {'Last month':>5}\n"
-    for name, costs in most_expensive[:max_entries]:
-        buffer += f"{name:{longest_name_len}} | {costs[0]:>12,.2f}$ | {cost_per_day_dict.get(name, [0])[0]:>8,.2f}$ | {(costs[1] if len(costs) > 1 else 0):.2f}$\n"
+    # render results
+    callout_users = []
+    for name, costs in personal_most_expensive:
+        if costs[0] > 400:
+            callout_users.append(name[23:-1])
 
     other_costs = [0.0] * 2
-    for _, costs in most_expensive[max_entries:]:
-        for i, cost in enumerate(costs):
-            other_costs[i] += cost
-    buffer += f"{'Other':{longest_name_len}} | {other_costs[0]:>12,.2f}$ |           | {other_costs[1]:.2f}$\n"
+    personal_buffer = f"{'AWS Accounts':^{longest_name_len}} | Month-to-date | yesterday | {'Last month':>5}\n"
+    for name, costs in personal_most_expensive:
+        last_month_cost = (costs[1] if len(costs) > 1 else 0)
+        if costs[0] > cost_floor or last_month_cost > cost_floor: 
+            personal_buffer += f"{name:{longest_name_len}} | {costs[0]:>12,.2f}$ | {cost_per_day_dict.get(name, [0])[0]:>8,.2f}$ | {last_month_cost:.2f}$\n"
+        else:
+            for i, cost in enumerate(costs):
+                other_costs[i] += cost
+    personal_buffer += f"{'Other':{longest_name_len}} | {other_costs[0]:>12,.2f}$ |           | {other_costs[1]:.2f}$\n"
 
-    return buffer
+    other_costs = [0.0] * 2
+    other_buffer = f"{'AWS Accounts':^{longest_name_len}} | Month-to-date | yesterday | {'Last month':>5}\n"
+    for name, costs in other_most_expensive:
+        last_month_cost = (costs[1] if len(costs) > 1 else 0)
+        if costs[0] > cost_floor or last_month_cost > cost_floor: 
+            other_buffer += f"{name:{longest_name_len}} | {costs[0]:>12,.2f}$ | {cost_per_day_dict.get(name, [0])[0]:>8,.2f}$ | {(costs[1] if len(costs) > 1 else 0):.2f}$\n"
+        else:
+            for i, cost in enumerate(costs):
+                other_costs[i] += cost
+    other_buffer += f"{'Other':{longest_name_len}} | {other_costs[0]:>12,.2f}$ |           | {other_costs[1]:.2f}$\n"
+
+    return personal_buffer, other_buffer, callout_users
 
 
-def publish_slack(hook_url, buffer):
+def publish_slack(hook_url: str, personal_buffer: str, other_buffer: str, callout_users: List[str]) -> None:
     resp = requests.post(
         hook_url,
         json={
-            "text": "```\n" + buffer + "\n```",
-        }
+           "text": "```\nPersonal Accounts\n\n" + personal_buffer + "\n\nOther Accounts\n\n" + other_buffer + "\n```",
+        },
     )
 
     if resp.status_code != 200:
         logger.warning("HTTP %s: %s" % (resp.status_code, resp.text))
+
+    if callout_users and datetime.datetime.now().isoweekday() == 2:
+        resp = requests.post(
+            hook_url,
+            json={
+                "text": "Expensive People: " + " ".join("<@" + user + ">" for user in callout_users), 
+            }
+        )
+        if resp.status_code != 200:
+            logger.warning("HTTP %s: %s" % (resp.status_code, resp.text))
